535dcb9416de86ce1f1f67f451b63f5d3be03d09
Rasmus Edvardsen <rasmusedvardsen@rasmuss-macbook-pro.local>
2018-07-07 23:39:38 +0200

    Initial Commit

---
+++ penguin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  penguin
+//
+//  Created by Rasmus Edvardsen on 07/07/2018.
+//  Copyright © 2018 penguin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
